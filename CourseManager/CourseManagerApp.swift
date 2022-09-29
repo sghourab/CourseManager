923896d3d2c0cb2cef7f3803c8169d6606a852e0
Samar Ghourab <samarghourab@gmail.com>
2022-09-29 10:11:44 -0400

    Initial Commit

---
+++ CourseManager/CourseManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseManagerApp.swift
+//  CourseManager
+//
+//  Created by Summer Crow on 2022-09-29.
+//
+
+import SwiftUI
+
+@main
+struct CourseManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
